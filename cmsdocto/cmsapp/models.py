a13cf4519339bd483e8b80b835387bd99950e8a7
shajahan tk <tshajahan494@gmail.com>
2024-06-03 11:00:45 +0530

    second

--- cmsdocto/cmsapp/models.py
+++ cmsdocto/cmsapp/models.py
@@ -67,7 +67,7 @@ class Add_patients(models.Model):
     Paitient_group= models.TextField(max_length=1000, null=True)
     Medical_condititon= models.CharField(max_length=1000, null=True)
     Type= models.CharField(max_length=100,null=True)
-    Consultation_fee= models.IntegerField(max_length=100,null=True)
+    Consultation_fee= models.IntegerField()
     Reffered_by= models.CharField(max_length=1000, null=True)
     Occupation= models.CharField(max_length=100, null=True)
     Photo= models.FileField()
@@ -98,6 +98,162 @@ class Clinicprofiles(models.Model):
     Time_zone= models.CharField(max_length=100)
     Pharmacy_licence_code= models.CharField(max_length=100)
 
+class Patientgroup(models.Model):
+    patientgroup=models.CharField(max_length=50,null=True)
+
+class Medicalcondition(models.Model):
+    medicalcondition=models.CharField(max_length=50,null=True)
+    
+
+class taxmodel(models.Model):
+    taxname=models.CharField(max_length=50)
+    taxpercentage=models.CharField(max_length=50)
+      
+class Paymentmethod(models.Model):
+    Payment_method=models.CharField(max_length=50,null=True)
+    discription=models.CharField(max_length=50,null=True)
+    
+class Documenttype(models.Model):
+    document_type=models.CharField(max_length=50)
+
+class Profilepic(models.Model):
+    profilepic=models.ImageField(upload_to='srs')
 
 
 
+class pharmacysupplier(models.Model):
+    suppilername=models.CharField(max_length=50)
+    companycode=models.IntegerField()
+    companyname=models.CharField(max_length=50)
+    contactname=models.CharField(max_length=50)
+    email=models.EmailField()
+    web=models.CharField(max_length=50)
+    phone=models.IntegerField()
+    address=models.CharField(max_length=100)
+    
+
+class pharmacyunit(models.Model):
+    unit=models.CharField(max_length=25)
+    
+
+class pharmacyitems(models.Model):
+    itemname=models.CharField(max_length=50)  
+    manufacture=models.CharField(max_length=50)
+    boxsize=models.CharField(max_length=50)
+    category=models.CharField(max_length=50)
+    purchase=models.CharField(max_length=50)
+    packingsize=models.CharField(max_length=50)
+    taxable=models.CharField(max_length=50)
+    recorder=models.CharField(max_length=50)
+    batchno=models.CharField(max_length=50)
+    salesrate=models.CharField(max_length=50)
+    deviceserialsnumber=models.CharField(max_length=50)
+    hsncode=models.CharField(max_length=50)
+
+class pharmacycategory(models.Model):
+    itemname=models.CharField(max_length=50)
+    manufacture=models.IntegerField()
+    description=models.CharField(max_length=50)
+
+class pharmacymanufacture(models.Model):
+    manufacture=models.CharField(max_length=50)
+    companycode=models.IntegerField()
+    companyname=models.CharField(max_length=50)
+    contactname=models.CharField(max_length=50)
+    email=models.EmailField()
+    web=models.CharField(max_length=50)
+    phone=models.IntegerField()
+    address=models.CharField(max_length=100)
+
+class pharmacysupplier(models.Model):
+    suppilername=models.CharField(max_length=50)
+    companycode=models.IntegerField()
+    companyname=models.CharField(max_length=50)
+    contactname=models.CharField(max_length=50)
+    email=models.EmailField()
+    web=models.CharField(max_length=50)
+    phone=models.IntegerField()
+    address=models.CharField(max_length=100)
+    
+
+class pharmacyunit(models.Model):
+    unit=models.CharField(max_length=25)
+    
+
+class pharmacyitems(models.Model):
+    itemname=models.CharField(max_length=50)  
+    manufacture=models.CharField(max_length=50)
+    boxsize=models.CharField(max_length=50)
+    category=models.CharField(max_length=50)
+    purchase=models.CharField(max_length=50)
+    packingsize=models.CharField(max_length=50)
+    taxable=models.CharField(max_length=50)
+    recorder=models.CharField(max_length=50)
+    batchno=models.CharField(max_length=50)
+    salesrate=models.CharField(max_length=50)
+    deviceserialsnumber=models.CharField(max_length=50)
+    hsncode=models.CharField(max_length=50)
+
+class pharmacycategory(models.Model):
+    itemname=models.CharField(max_length=50)
+    manufacture=models.IntegerField()
+    description=models.CharField(max_length=50)
+
+class pharmacymanufacture(models.Model):
+    manufacture=models.CharField(max_length=50)
+    companycode=models.IntegerField()
+    companyname=models.CharField(max_length=50)
+    contactname=models.CharField(max_length=50)
+    email=models.EmailField()
+    web=models.CharField(max_length=50)
+    phone=models.IntegerField()
+    address=models.CharField(max_length=100)
+
+
+    # pathology ///
+
+class header(models.Model):
+    suppliername=models.CharField(max_length=50)
+
+
+class Addlab(models.Model):
+    Lab_name= models.CharField(max_length=100)
+    Lab_code=models.CharField(max_length=100)
+    Company_name= models.CharField(max_length=100)
+    Contact_name=models.CharField(max_length=100)
+    Email= models.EmailField()
+    Web= models.CharField(max_length=1000)
+    Phone= models.CharField(max_length=1000)
+    Address= models.CharField(max_length=100)
+
+class Addlabtest(models.Model):
+    Test_no= models.IntegerField()
+    Test_description= models.CharField(max_length=100)
+    Test_charge= models.IntegerField()
+    Carry_out= models.CharField(max_length=100)
+    Report= models.TextField(max_length=1000)
+    Test_type= models.CharField(max_length=100)
+    Report_head= models.CharField(max_length=1000)
+
+class Addlabtestpakeges(models.Model):
+    Item_name= models.CharField(max_length=1000)
+    Manufacture_date= models.DateField()
+    Description= models.TextField(max_length=1000)
+
+class Addlabtestdeatail(models.Model):
+    labtest_detail=models.CharField(max_length=100)
+
+# dental //
+
+class Dentalwork(models.Model):
+    Workname=models.CharField(max_length=50)
+    Shade=models.CharField(max_length=50)
+    Worktype=models.CharField(max_length=50)
+    Alloytype=models.CharField(max_length=50)    
+
+class Addlaboratories(models.Model):
+    Labname= models.CharField(max_length=100)   
+    Labtype= models.CharField(max_length=100)
+    Phone=models.CharField(max_length=50)
+    Executive=models.CharField(max_length=50)
+    Address=models.CharField(max_length=50)
--- cmsdocto/cmsapp/views.py
+++ cmsdocto/cmsapp/views.py
@@ -8,7 +8,6 @@ from django.contrib import messages
 
 def home(request):
     return redirect('/login')
-
 def signup(request):
     return render(request,'signup.html')
 def dash(request):
@@ -18,7 +17,7 @@ def login_form(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
-        if user is not None:
+        if user :
             login(request, user)
             messages.success(request, 'You are now logged in.')
             return redirect('/dash')  # Redirect to '/dash'
@@ -26,50 +25,38 @@ def login_form(request):
             messages.error(request, 'Invalid username or password.')
             return redirect('/login')  # Redirect to '/login'
     return render(request, 'login.html')
-
-
-    
-
-
-
 def newpatient1(request):
     return render(request,'newpatient1.html')
-
 def todayappointment(request):
     apmnts=Add_patients.objects.all()
     return render(request,'todayappointment.html',{'apmnts':apmnts})
-
 def withpatient(request):
     return render(request,'withpatient.html')
-
 def fastrack(request):
     return render(request,'fastrack.html')
-
 def appointment(request):
     return render(request,'appointment.html')
-
 def search(request):
     searchpatient=Add_patients.objects.all()
     return render(request,'search.html',{'patient':searchpatient})
-
 def ehrreports(request):
     return  render(request,'ehrreports.html')
-
 def telemedicine(request):
     tele=Add_patients.objects.all()
     return render(request,'telemedicine.html',{'telemd':tele})
-
 def settings(request):
     return render(request,'settings.html')
-
 def sample1(request):
     return render(request,'sample1.html')
-
 def clinicsettings(request):
     return render(request,'clinicsettings.html')
-
 def generalsettings(request):
-    return render(request,'generalsettings.html')
+    ptnt=Patientgroup.objects.all()
+    mdcl=Medicalcondition.objects.all()
+    taxstngs=taxmodel.objects.all()
+    pymnt=Paymentmethod.objects.all()
+    dcmt=Documenttype.objects.all()
+    return render(request,'generalsettings.html',{'ptnt':ptnt,'mdcl':mdcl,'taxstngs':taxstngs,'pymnt':pymnt,'dcmt':dcmt})
 def clinciprofile(request):
     if request.method=='POST':
         clinic_name= request.POST['clinic_name']
@@ -95,6 +82,39 @@ def clinciprofile(request):
         c= Clinicprofiles(Clinic_name= clinic_name, Phone_no= phone_no, Address=address, Locality=locality, State=state, Email=email, Tax=tax, Default_language=default_language, Date_start=date_start, Pharmacy_name= pharmacy_name, Specialization=specialization, Mobile_no=mobile_no1,Slogan=slogan, Country=country, City= city, Website=website, Zip=zip, Default_Currency=default_currency, Time_zone=time_zone, Pharmacy_licence_code=pharmacy_licence_code)
         c.save()
         return redirect('/generalsettings')
+def patientgroup1(request):
+    if request.method =='POST':
+        patientgroup=request.POST['patientgroup']
+        ptnt=Patientgroup(patientgroup=patientgroup)
+        ptnt.save()
+        return redirect('/generalsettings')
+def medicalconditions(request):
+    if request.method =='POST':
+        medicalcondition=request.POST['medicalcondition'] 
+        mdcl=Medicalcondition(medicalcondition=medicalcondition)
+        mdcl.save()
+        return redirect('/generalsettings')    
+def taxsettings(request):
+    if request.method =='POST':
+        taxname=request.POST['taxname']  
+        taxpercentage=request.POST['taxpercentage'] 
+        tax=taxmodel(taxname=taxname, taxpercentage=taxpercentage)
+        tax.save()
+        return redirect('/generalsettings')   
+def paymentmethod(request):
+    if request.method =='POST':
+        paymentmethod=request.POST['paymentmethod']
+        discription=request.POST['discription1']
+        pymnt=Paymentmethod(Payment_method=paymentmethod,discription=discription)
+        pymnt.save()
+        return redirect('/generalsettings')   
+
+def documenttype(request):
+    if request.method =='POST':
+        documenttype=request.POST['documenttype']
+        dcmt=Documenttype(document_type=documenttype)
+        dcmt.save()
+        return redirect('/generalsettings')
 
 
 
@@ -114,7 +134,30 @@ def pathology(request):
     return render(request,'pathology.html')
 
 def dental(request):
-    return render(request,'dental.html')
+    dentalwork=Dentalwork.objects.all()
+    addlaboratory=Addlaboratories.objects.all()
+    return render(request,'dental.html',{'dentalwork':dentalwork,'addlaboratory':addlaboratory})
+
+def dentalworks(request):
+    if request.method =='POST':
+        workname=request.POST['workname']
+        shade=request.POST['shade']
+        worktype=request.POST['worktype']
+        alloytype=request.POST['alloytype']
+        dntl=Dentalwork(Workname=workname,Shade=shade,Worktype=worktype,Alloytype=alloytype)
+        dntl.save()
+        return redirect('/dental')
+    
+def addlaboratories(request):
+    if request.method == 'POST':
+        labname=request.POST['labname']
+        labtype=request.POST['labtype']
+        phone=request.POST['phone']
+        executive=request.POST['executive']
+        address=request.POST['address']
+        lbrtry=Addlaboratories(Labname=labname,Labtype=labtype,Phone=phone,Executive=executive,Address=address)
+        lbrtry.save()
+        return redirect('/dental')
 
 def communication(request):
     cmnctn=Add_patients.objects.all()   
@@ -281,10 +324,156 @@ def newpatient(request):
         reffered_by= request.POST['reffered_by']
         occupation= request.POST['occupation']
         attachments= request.POST['attachments']
-        doctor= request.POST['doctor']
-        addpaitent= Add_patients.objects.create(Register_id= registration_id, Category=fname, First_name=first_name, Last_name=last_name, Record_no=rec_no, Mobile_no=mobile_no, Email=email, State=state, City=city, Address=address, Date_of_birth=date_of_birth, Age=age, Phone_no=phone_no, Guardian_name=guardian_name, Op_no=op_no, Passport_no=passport_no, Date_of_discharge=date_of_discharege, Gender=gender, Blood_group=blood_group, Medical_history=medical_histoy, Paitient_group=paitient_group, Medical_condititon=medical_condition, Type=type, Consultation_fee=consultation_fee, Reffered_by=reffered_by, Occupation=occupation, Photo=attachments, Doctor=doctor, Country=country,  Paitient_remark=paitient_remark)
+        doctor1= request.POST['doctor1']
+        addpaitent= Add_patients.objects.create(Register_id= registration_id, Category=fname, First_name=first_name, Last_name=last_name, Record_no=rec_no, Mobile_no=mobile_no, Email=email, State=state, City=city, Address=address, Date_of_birth=date_of_birth, Age=age, Phone_no=phone_no, Guardian_name=guardian_name, Op_no=op_no, Passport_no=passport_no, Date_of_discharge=date_of_discharege, Gender=gender, Blood_group=blood_group, Medical_history=medical_histoy, Paitient_group=paitient_group, Medical_condititon=medical_condition, Type=type, Consultation_fee=consultation_fee, Reffered_by=reffered_by, Occupation=occupation, Photo=attachments, Doctor=doctor1, Country=country,  Paitient_remark=paitient_remark)
         addpaitent.save()
         return render(request, 'newpatient1.html')
     
 def expireddoc(request):
     return render(request,'expireddoc.html')
+
+def profile(request):
+    profilepic=Profilepic.objects.all()
+    return render(request,'profile.html',{'profile':profilepic})
+
+def profilepic(request):
+    if request.method == 'POST':
+        profilepic = request.POST['profile_pic']
+        profile=Profilepic(profilepic=profilepic)
+        profile.save()
+        return redirect('/profile')
+    
+def consentform(request):
+    return render(request,'consentform.html')
+def certificate(request):
+    return render(request,'certificateform.html')
+
+
+def pharmacysettings(request):
+    supplier=pharmacysupplier.objects.all()
+    phunit=pharmacyunit.objects.all()
+    phitem=pharmacyitems.objects.all()
+    phcat=pharmacycategory.objects.all()
+    phman=pharmacymanufacture.objects.all()
+    return render(request,'pharmacysettings.html',{'supplier':supplier,'pharmacyunit':phunit,'phitem':phitem,'phcat':phcat,'phman':phman})
+def pharmacysupply(request):
+    if request.method == 'POST':
+        suppilername=request.POST['Suppliername']
+        companycode=request.POST['companycode']
+        companyname=request.POST['companyname']
+        contactname=request.POST['contactname']
+        email=request.POST['email']
+        web=request.POST['web']
+        phone=request.POST['phone']
+        address=request.POST['address']
+        address=request.POST['address']
+        phr=pharmacysupplier(suppilername=suppilername,companycode=companycode,companyname=companyname,contactname=contactname,email=email,web=web,phone=phone,address=address)
+        phr.save()
+        return redirect('/pharmacysettings')
+
+def pharmacyuni(request):
+    if request.method == 'POST':
+        unit=request.POST['unit']
+        hey=pharmacyunit(unit=unit)
+        hey.save()
+        return redirect('/pharmacysettings')
+def pharmacyitem(request):
+    if request.method=='POST':
+        itemname=request.POST['item name']
+        manufacture=request.POST['manufacture']
+        boxsize=request.POST['boxsize']
+        category=request.POST['category']
+        purchaserate=request.POST['purchase']
+        packingsize=request.POST['packingsize']
+        taxable=request.POST['taxable']
+        recorderquantity=request.POST['recorder']
+        batchno=request.POST['batchno']
+        salesrate=request.POST['salesrate']
+        deviceserialnumber=request.POST['deviceserialsnumber']
+        hsncode=request.POST['hsncode']
+        phritem=pharmacyitems(itemname=itemname,manufacture=manufacture,boxsize=boxsize,category=category,purchase=purchaserate,packingsize=packingsize,taxable=taxable,recorder=recorderquantity,batchno=batchno,salesrate=salesrate,deviceserialsnumber=deviceserialnumber,hsncode=hsncode)
+        phritem.save()
+        return redirect('/pharmacysettings')
+def categories(request):
+    if request.method == 'POST':
+        itemname = request.POST['itemname']
+        manufacture = request.POST['manufacture']
+        description = request.POST['description']
+        cat=pharmacycategory(itemname=itemname,manufacture=manufacture,description=description)
+        cat.save()
+        return redirect('/pharmacysettings')
+def manufacture(request):
+    if request.method =='POST':
+        manufacture=request.POST['manufacture']
+        companycode=request.POST['companycode']
+        companyname=request.POST['companyname']
+        contactname=request.POST['contactname']
+        email=request.POST['email']
+        web=request.POST['web']
+        phone=request.POST['phone']
+        address=request.POST['address']
+        man=pharmacymanufacture(manufacture=manufacture,companycode=companycode,companyname=companyname,contactname=contactname,email=email,web=web,phone=phone,address=address)
+        man.save()
+        return redirect('/pharmacysettings')
+    # pathology ///
+def pathology(request):
+    header1=header.objects.all()
+    lab1=Addlab.objects.all()
+    lab2=Addlabtest.objects.all()
+    lab3=Addlabtestpakeges.objects.all()
+    return render(request,'pathology.html',{'header':header1,'addlab':lab1,'addlabtest':lab2,'items':lab3})
+def Header(request):
+    if request.method=='POST':
+        suppliername=request.POST['suppliername']
+        sup=header(suppliername=suppliername)
+        sup.save()
+        return redirect('/pathology')
+def addlab(request):
+    if request.method=='POST':
+        labname=request.POST['lab_name']
+        labcode= request.POST['lab_code']
+        companyname= request.POST['company_name']
+        contactname= request.POST['contact_name']
+        email= request.POST['email']
+        web= request.POST['web']
+        phone= request.POST['phone']
+        address= request.POST['address']
+        lab=Addlab(Lab_name=labname,Lab_code=labcode,Company_name=companyname,Contact_name=contactname,Email=email,Web=web,Phone=phone,Address=address)
+        lab.save()
+        return redirect('/pathology')
+def addlabtest(request):
+    if request.method=='POST':
+        testno= request.POST['test_no']
+        testdescription= request.POST['test_description']
+        testcharge= request.POST['test_charge']
+        carry_out= request.POST['carry_out']
+        report= request.POST['report']
+        testype= request.POST['test_type']
+        reporthead= request.POST['report_head']
+        labtest=Addlabtest(Test_no=testno,Test_description=testdescription,Test_charge=testcharge,Carry_out=carry_out,Report=report,Test_type=testype,Report_head=reporthead)
+        labtest.save()
+        return redirect('/pathology')   
+def addlabtestpakages(request):
+    if request.method =='POST':
+        itemname= request.POST['item_name']
+        manufaturedate= request.POST['manufacture_date']
+        description= request.POST['description']
+        pakages= Addlabtestpakeges(Item_name=itemname,Manufacture_date=manufaturedate,Description=description)
+        pakages.save()
+        return redirect('/pathology')
+def addlabtestdetails(request):
+    if request.method =='POST':
+        labtest_detail= request.POST['labtest_detail']
+        lb=Addlabtestdeatail(labtest_detail=labtest_detail)
+        lb.save()
+        return redirect('/pathology')  
+def vacination(request):
+    return render(request, 'vacinationlist.html')
+def opcard(request):
+    return render(request,'opcard.html')
+def rolesandprivileges(request):
+    return render(request,'rolesandprivileges.html')
+def staffdetails(request):
+    return render(request,'staffdetails.html')
+def patientdetails(request):
+    return render(request,'patientdetails.html')
